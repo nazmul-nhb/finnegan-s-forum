9d3335517fd57fdd119363cf06ce572f92d56052
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-03 19:20:50 +0600

    enabled mark-as-read button

--- scripts/forum.js
+++ scripts/forum.js
@@ -9,10 +9,10 @@ const allPostsContainer = document.getElementById('all-posts-container');
 
 const displayAllPosts = (posts) => {
     posts.forEach(post => {
-        console.log(post);
+        // Displaying all the posts from API
         const postCard = document.createElement('div');
         postCard.className = 'w-full bg-[#797DFC1A] p-4 lg:p-10 rounded-3xl flex flex-col lg:flex-row justify-start gap-6';
-        postCard.innerHTML =`
+        postCard.innerHTML = `
                             <div class="w-[72px] relative">
                                 <img src="${post.image}" alt="Profile Picture">
                                 <img class="absolute -top-1 -right-1" src="images/online.svg" alt="online-offline">
@@ -27,26 +27,32 @@ const displayAllPosts = (posts) => {
                                 <hr class="border border-dashed border-[#12132D40]">
                                 <div class="flex justify-between items-center">
                                     <div class="flex justify-start items-center gap-3 lg:gap-7 text-[#12132D99]">
-                                        <div class=""><i class="fa-regular fa-message"></i> ${post.comment_count}</div>
-                                        <div class=""><i class="fa-regular fa-eye"></i> ${post.view_count} </div>
-                                        <div class=""><i class="fa-regular fa-clock"></i> ${post.posted_time} min</div>
+                                        <div><i class="fa-regular fa-message"></i> ${post.comment_count}</div>
+                                        <div><i class="fa-regular fa-eye"></i> ${post.view_count} </div>
+                                        <div><i class="fa-regular fa-clock"></i> ${post.posted_time} min</div>
                                     </div>
-                                    <div class="">
-                                        <button
-                                            class="w-7 h-7 bg-[#10B981] border border-[#10B981] text-base text-white hover:bg-white hover:text-[#10B981] rounded-full"><i
-                                                class="fa-solid fa-envelope-open"></i></button>
+                                    <div>
+                                        <button onclick="markAsRead('${post.title}', ${post.view_count})" id="mark-as-read" class="w-7 h-7 bg-[#10B981] border border-[#10B981] text-base text-white hover:bg-white hover:text-[#10B981] rounded-full"><i class="fa-solid fa-envelope-open"></i></button>
                                     </div>
                                 </div>
-                            </div>        
-        `;
+                            </div>`;
         allPostsContainer.appendChild(postCard);
-
-});
-
-
+    });
 }
 
 
+// Enable Mark as Read Button
+const markAsRead = (title, viewCount) => {
+    console.log(title, viewCount);
+    const markedAsReadContainer = document.getElementById('marked-as-read-container');
+    const readPostCard = document.createElement('div');
+    readPostCard.className = 'bg-white rounded-2xl p-2 lg:p-4 flex justify-between items-center flex-1 gap-2';
+    readPostCard.innerHTML = `
+                <h4 class="text-sm lg:text-xl font-semibold w-2/3">${title}</h4>
+                <h5 class="text-[#12132D99] text-sm lg:text-base"><i class="fa-regular fa-eye"></i> ${viewCount}</h5>
+            `;
+    markedAsReadContainer.appendChild(readPostCard);
+};
 
 
 
